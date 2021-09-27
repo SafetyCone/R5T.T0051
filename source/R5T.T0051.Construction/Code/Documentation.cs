d7d2dee30bbd660e3b662ba0e45637284ddd0e64
davidcoats <david@getsafetycone.com>
2021-09-27 14:51:07 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0051.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0051.Construction
+{
+    /// <summary>
+    /// Construction project for TypeCodeLocationIndex and TypeCodeLocationIndexOperator..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0051/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0051
+{
+    /// <summary>
+    /// TypeCodeLocationIndex and TypeCodeLocationIndexOperator.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
